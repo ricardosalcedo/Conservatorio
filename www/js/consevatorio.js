e029600cf6a72b83c9774535785b3054496c4fe8
ricardosalcedo <ricardo.salcedo@gmail.com>
2015-01-29 22:13:49 -0500

    Responsive changes

--- www/js/consevatorio.js
+++ www/js/consevatorio.js
@@ -1,5 +1,15 @@
-$(function() {
-        
-        
-        
-        });
+$('body').on('tap', 'a', function(e) {
+    window.location = $(this).attr('href');
+    e.preventDefault();
+});
+
+$('#page1').on('flick', function(e) {
+    if ('horizontal' == e.orientation) {
+        if (1 == e.direction) {
+            $(this).addClass('is-opened');
+        }
+        else {
+            $(this).removeClass('is-opened');
+        }
+    }
+});
