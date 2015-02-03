39d393d2c52d85e99e5404a5ab5a9161f1a5aa0a
ricardosalcedo <ricardo.salcedo@gmail.com>
2015-02-02 22:30:20 -0500

    Mayor updates

--- platforms/ios/www/js/consevatorio.js
+++ platforms/ios/www/js/consevatorio.js
@@ -3,10 +3,27 @@ $('body').on('tap', 'a', function(e) {
     e.preventDefault();
 });
 
+$(document).bind("mobileinit", function(){
+    $.mobile.defaultPageTransition = 'none'; 
+    $.mobile.pushStateEnabled = false;  
+  });
+
 $(document).on("pageinit","#page1",function(){
     $("#page1").on("swipeleft",function(){
-        $("#page1").liquidSlider($.mobile.navigate( "#page2" ));
+        $.mobile.navigate( "#page2", { transition : "slide"} );
     });
+    
+window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '707742835999981',
+          cookie     : true,  // enable cookies to allow the server to access 
+                              // the session
+          xfbml      : true,  // parse social plugins on this page
+          version    : 'v2.1' // use version 2.1
+        });
+
+        };
+    
 });
 
 // This is called with the results from from FB.getLoginStatus().
@@ -41,32 +58,24 @@ function checkLoginState() {
   });
 }
 
-window.fbAsyncInit = function() {
-FB.init({
-  appId      : '707742835999981',
-  cookie     : true,  // enable cookies to allow the server to access 
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.1' // use version 2.1
-});
+function logIn(){
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+          // Logged into your app and Facebook.
+        } else if (response.status === 'not_authorized') {
+          // The person is logged into Facebook, but not your app.
+        } else {
+          // The person is not logged into Facebook, so we're not sure if
+          // they are logged into this app or not.
+        }
+      });
+}
 
-// Now that we've initialized the JavaScript SDK, we call 
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
-
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
-
-};
+function logOut(){
+    FB.logout(function(response) {
+        $.mobile.navigate( "#page2" );
+    });
+}
 
 // Load the SDK asynchronously
 (function(d, s, id) {
@@ -81,7 +90,7 @@ FB.getLoginStatus(function(response) {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
+  FB.api('/me', function(response) {  
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
--- platforms/ios/www/js/load-json.js
+++ platforms/ios/www/js/load-json.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
 		timeout: 5000,
 		success: function(data, status){
 		    console.log(status);
-		    if(data != undefined && data.post != undefined){
+		    //if(data != undefined && data.post != undefined){
     			$.each(data, function(i,item){
     				var landmark = '<br><br><a href="#page7" class="ui-btn ui-btn-inline" id="not-but"><div class="info"><label class="date">'+item.Fecha+'</label><span class="ttl">'
-    				+item.Nombre+'</span></div><div id="imgnot" src="../img/'+item.Foto+'"></div>';
+    				+item.Nombre+'</span></div><div id="imgnot" src="http://smdevelopers.co/smdev/Conservatorio/protected/banner/'+item.Foto+'"></div>';
     				
     				output.append(landmark);
     			});
-		    }
+		    //}
 		},
 		error: function(){
 			alert('Hubo un error al cargar los datos');
