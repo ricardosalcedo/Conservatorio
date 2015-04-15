2dc8f1fdb93acb9de45a393905db25fec6ccb229
ricardosalcedo <ricardo.salcedo@gmail.com>
2015-04-14 23:40:00 -0500

    FB

--- platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.m
+++ platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.m
@@ -206,7 +206,6 @@ static NSString *stripFragment(NSString* url)
 
 - (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
 {
-    
     BOOL shouldLoad = YES;
 
     if ([_delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)]) {
